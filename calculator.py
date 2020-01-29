5cc6c0af80d721c13f148fcbf9e0a181b7578808
SNEHA1962000 <60418764+SNEHA1962000@users.noreply.github.com>
2020-01-29 10:56:58 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a,b):
+     add=a+b
+     print(add)
+add(3,5)
+def sub(a,b):
+    sub=a+b
+    print(sub)
+sub(3,5)
+def mul(a,b):
+   mul=a*b
+   print(mul)
+mul(3,5)
+def div(a,b):
+   div=a/b
+   print(div)
+div(3,5)
